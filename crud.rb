ba1601688997cb88a54f3fdd60e0a2089b82995d
BarbaraCristinaNunes <nunes.barbarac@gmail.com>
2023-02-01 18:04:07 +0100

    created method to get pokemon's evolutions

--- crud.rb
+++ crud.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 require 'uri';
 require 'net/http';
-
+require 'json';
 module Crud
   PATH = 'https://pokeapi.co/api/v2/pokemon/'
   def get_pokemonByIdCrud(id)
     res = Net::HTTP.get_response(URI(PATH + id));
-    puts res.body if res.is_a?(Net::HTTPSuccess)
+    JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+  end
+  def get_evolutionsCrud(name)
+    res = Net::HTTP.get_response(URI(PATH + name));
+    JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
   end
 end
