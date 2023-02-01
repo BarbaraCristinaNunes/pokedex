d955328c345183da977f4279bee5a09d928914fd
BarbaraCristinaNunes <nunes.barbarac@gmail.com>
2023-02-01 18:06:27 +0100

    created methods get_name, get_types, get_abilities and get_evolutions. Only get_evolutions is not working yet

--- pokemon.rb
+++ pokemon.rb
@@ -4,10 +4,51 @@ require_relative 'crud.rb'
 class Pokemon
   include Crud;
   def initialize(id)
-    #@pokemon= pokemon;
     @id = id;
+    pokemon = self.get_pokemonByIdCrud(@id);
+    @pokemon = pokemon;
+    @name = @pokemon['species']['name'];
+    @types = [];
+    @powers = [];
+    @evolutions = [];
   end
-  def get_pokemonById
-    self.get_pokemonByIdCrud(@id)
+  def get_name
+    @name;
+  end
+
+  def get_types
+    for type in @pokemon['types']
+      @types << type['type']['name'];
+    end
+    @types
+  end
+
+  def get_abillities
+    for ability in @pokemon['abilities']
+      @powers << ability['ability']['name'];
+    end
+    @powers;
+  end
+
+  def get_evolutions
+    evolutions = self.get_evolutionsCrud(@name);
+    @evolutions << evolutions['species']['name'];
+
+    if evolutions["evolves_to"]
+
+      evolutions["evolves_to"].each { |evolution|
+
+        @evolutions << evolution['species']["name"];
+
+        if evolution["evolves_to"]
+
+          evolution["evolves_to"].each { |evolution|
+            @evolutions << evolution['species']["name"];
+          }
+
+        end
+      }
+    end
+    @evolutions;
   end
 end
