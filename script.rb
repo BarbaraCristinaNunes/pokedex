691fc1364e578706e211781c93f84f9ce6b71b8f
BarbaraCristinaNunes <nunes.barbarac@gmail.com>
2023-02-01 18:05:15 +0100

    now I can show name, types and abilities of pokemon

--- script.rb
+++ script.rb
@@ -4,57 +4,32 @@ require_relative 'pokemon';
 
 
 module Script
-  Pokemons = [
-    {
-      :id => 1,
-      :name => "bulbasaur",
-      :types =>[
-        "grass",
-        "poison"
-      ],
-      :favorit => false,
-      #:image => "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-      :powers => [
-        "Overgrow",
-        "Chlorophyll"
-      ],
-      :evolutions => [
-        "bulbasaur",
-        "ivysaur",
-        "venusaur"
-      ]
-    },
-    {
-      :id => 2,
-      :name => "ivysaur",
-      :types => [
-        "grass",
-        "poison"
-      ],
-      :favorit => false,
-      #:image => "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
-      :powers => [
-        "Overgrow",
-        "Chlorophyll"
-      ],
-      :evolutions => [
-        "bulbasaur",
-        "ivysaur",
-        "venusaur"
-      ]
-    },
-  ];
   #uri = URI('https://pokeapi.co/api/v2/pokemon/1')
   #res = Net::HTTP.get_response(uri)
   #puts res.body if res.is_a?(Net::HTTPSuccess)
 
   #puts response.body if response.code == 200
-  pk = Pokemon.new("1")
-  puts pk.get_pokemonById
 
+  pokemon = Pokemon.new("1")
+  myPokemon = {
+  # :id => pokemon['id'],
+    :name => pokemon.get_name,
+    :types => pokemon.get_types,
+    :powers => pokemon.get_abillities,
+    :evolutions => pokemon.get_evolutions,
+  }#:name => pokemon['species']['name'],
+  #or type in pokemon['types']
+   #myPokemon[:types] << type['type']['name']
+# end
+# for ability in pokemon['abilities']
+#   myPokemon[:powers] << ability['ability']['name']
+ #end
+
+puts pokemon.get_name
+
+  puts myPokemon;
   #for pokemon in Pokemons
   #pk = Pokemon.new(pokemon);
   #puts pk.get_pokemon;
   #end
-
 end
