54019c893d353d6cbee6201f270e43bac486b39f
BarbaraCristinaNunes <nunes.barbarac@gmail.com>
2023-02-02 15:49:05 +0100

    it is done

--- script.rb
+++ script.rb
@@ -1,35 +1,18 @@
 # frozen_string_literal: true
-require_relative 'pokemon';
-
-
-
+#require_relative 'pokemon';
+require_relative 'pokemon_service'
 module Script
-  #uri = URI('https://pokeapi.co/api/v2/pokemon/1')
-  #res = Net::HTTP.get_response(uri)
-  #puts res.body if res.is_a?(Net::HTTPSuccess)
 
-  #puts response.body if response.code == 200
-
-  pokemon = Pokemon.new("1")
+  service = PokemonService::PokemonService.new
+  pokemon = service.get_pokemon("eevee")
   myPokemon = {
-  # :id => pokemon['id'],
+    :id => pokemon.get_id,
     :name => pokemon.get_name,
     :types => pokemon.get_types,
-    :powers => pokemon.get_abillities,
+    :powers => pokemon.get_abilities,
     :evolutions => pokemon.get_evolutions,
-  }#:name => pokemon['species']['name'],
-  #or type in pokemon['types']
-   #myPokemon[:types] << type['type']['name']
-# end
-# for ability in pokemon['abilities']
-#   myPokemon[:powers] << ability['ability']['name']
- #end
-
-puts pokemon.get_name
+  }
 
   puts myPokemon;
-  #for pokemon in Pokemons
-  #pk = Pokemon.new(pokemon);
-  #puts pk.get_pokemon;
-  #end
+
 end
