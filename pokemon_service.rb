cb39e54799df4a6686e47636bdecab0fbb38cd57
BarbaraCristinaNunes <nunes.barbarac@gmail.com>
2023-02-02 15:47:33 +0100

    created private methods to deal with types, abilities and evolutions of pokemon

--- pokemon_service.rb
+++ pokemon_service.rb
@@ -1,14 +1,55 @@
 # frozen_string_literal: true
-#require 'uri';
+# require 'uri';
 require 'net/http';
 require 'json';
-module Crud
-  def get_pokemonByIdCrud(id)
-    res = Net::HTTP.get_response(URI('https://pokeapi.co/api/v2/pokemon/'+ id));
-    JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess);
-  end
-  def get_evolutionsCrud(id, name)
-    res = Net::HTTP.get_response(URI('https://pokeapi.co/api/v2/pokemon-species/' + name));
-    JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess);
+require_relative 'pokemon'
+module PokemonService
+  class PokemonService
+    def get_pokemon(input)
+      res = Net::HTTP.get_response(URI('https://pokeapi.co/api/v2/pokemon/' + input.to_s));
+      json = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess);
+      pokemon = Pokemon.new(json);
+      get_type(pokemon, json)
+      get_abilities(pokemon, json)
+      get_evolutions(pokemon, json)
+      pokemon
+    end
+
+
+    private
+    def get_type(pokemon, json)
+      array = []
+      json['types'].each { |type| array << type['type']['name'] }
+      pokemon.set_types = array
+    end
+    def get_abilities(pokemon, json)
+      array = []
+      json['abilities'].each { |ability| array << ability['ability']['name'] }
+      pokemon.set_abilities = array
+    end
+    def get_evolutions(pokemon, json)
+      pathToEvolutions = Net::HTTP.get_response(URI(json['species']['url']));
+      evolutionChainUrl = JSON.parse(pathToEvolutions.body)['evolution_chain']['url'] if pathToEvolutions.is_a?(Net::HTTPSuccess)
+
+      res = Net::HTTP.get_response(URI(evolutionChainUrl))
+      evolutions = JSON.parse(res.body)['chain'] if res.is_a?(Net::HTTPSuccess)
+
+      array = []
+      array << evolutions['species']['name'];
+
+      if evolutions["evolves_to"]
+
+        evolutions["evolves_to"].each do |evolution|
+
+          array << evolution['species']["name"];
+
+          if evolution["evolves_to"]
+            evolution["evolves_to"].each { |evolution| array << evolution['species']["name"]; }
+          end
+        end
+      end
+      pokemon.set_evolutions = array
+    end
+
   end
 end
