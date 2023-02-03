4061ec5ec09e8f6a47edc0ddaece03b6596e0519
BarbaraCristinaNunes <nunes.barbarac@gmail.com>
2023-02-03 15:52:16 +0100

    removed getters and setters

--- pokemon.rb
+++ pokemon.rb
@@ -1,35 +1,11 @@
 # frozen_string_literal: true
 class Pokemon
+  attr_accessor :types, :abilities, :evolutions, :name, :id
   def initialize(json)
     @id = json['id']
     @name = json['species']['name']
     @types = []
     @powers = []
     @evolutions = []
-  end
-
-  def get_id
-    @id
-  end
-  def get_name
-    @name
-  end
-  def get_types
-    @types
-  end
-  def set_types=(array)
-    @types = array
-  end
-  def get_abilities
-    @powers
-  end
-  def set_abilities=(array)
-    @powers = array
-  end
-  def get_evolutions
-    @evolutions
-  end
-  def set_evolutions=(array)
-    @evolutions = array
   end
 end
