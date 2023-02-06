bd8ad8b83fa0324564ff2f5968354c18195d2adc
BarbaraCristinaNunes <nunes.barbarac@gmail.com>
2023-02-06 12:08:40 +0100

    now pokemon are showed using methos pokemon from class pokemon

--- script.rb
+++ script.rb
@@ -4,15 +4,6 @@ module Script
 
   service = PokemonService::PokemonService.new
   pokemon = service.get_pokemon_by_name("wurmple")
-
-  myPokemon = {
-    :id => pokemon.id,
-    :name => pokemon.name,
-    :types => pokemon.types,
-    :powers => pokemon.abilities,
-    :evolutions => pokemon.evolutions,
-  }
-
-  puts myPokemon
+  puts pokemon.pokemon
 
 end
